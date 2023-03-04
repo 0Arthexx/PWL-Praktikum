fccce59d34a4ef319b9138db7ff8dcf67fe02596
tabithaek14 <90822972+tabithaek14@users.noreply.github.com>
2023-03-05 02:43:18 +0900

    Add files via upload

---
+++ book.php
@@ -0,0 +1,44 @@
+<?php
+$link = new PDO('mysql:host=localhost;dbname=pwl2022', 'root', '');
+$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
+$query = 'SELECT isbn, title, author, publisher, publisher_year, genre_id FROM book';
+$stmt = $link->prepare($query);
+$stmt->execute();
+$results = $stmt->fetchAll();
+$link = null;
+?>
+<style>
+    tr {
+        background-color: #FFDD59;
+    }
+</style>
+
+<div class="container mt-5 ">
+    <table class="table mt-3 text-center" id="example">
+        <thead>
+        <tr>
+            <th class="text-center">ISBN</th>
+            <th class="text-center">Title</th>
+            <th class="text-center">Author</th>
+            <th class="text-center">Publisher</th>
+            <th class="text-center">Publisher Year</th>
+            <th class="text-center">Genre ID</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($results as $book) {
+            echo '<tr>';
+            echo '<td>' . $book['isbn'] . '</td>';
+            echo '<td>' . $book['title'] . '</td>';
+            echo '<td>' . $book['author'] . '</td>';
+            echo '<td>' . $book['publisher'] . '</td>';
+            echo '<td>' . $book['publisher_year'] . '</td>';
+            echo '<td>' . $book['genre_id'] . '</td>';
+            echo '</tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap5.min.css">
+
+</head>
+<body>
+    <style>
+        .warna {
+            background-color: #9ADCFF;
+        }
+        li {
+            font-family: "Javanese Text";
+        }
+        .text {
+            font-family: "Javanese Text";
+            margin-top: 20px;
+            margin-left: 10px;
+        }
+
+    </style>
+
+    <nav class="navbar navbar-expand-lg sticky-top warna">
+        <div class="container-fluid m-2">
+            <img src="./src/logofixed.png" alt="" width="50">
+            <h4 class="text" >BACA.KUY</h4>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="?menu=home"> Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="?menu=genre"> Genre </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="?menu=book"> Book </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main>
+        <?php
+        $navigation = filter_input(INPUT_GET, var_name: 'menu');
+        switch ($navigation) {
+            case 'home':
+                include_once 'home.php';
+                break;
+            case 'genre':
+                include_once 'genre.php';
+                break;
+            case 'book':
+                include 'book.php';
+                break;
+            default:
+                include_once 'home.php';
+                break;
+            }
+            ?>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#example').DataTable();
+        });
+    </script>
+</body>
+</html>
